6c1f42288b0243f41c9c01540700610ec42a7f3f
Shubhang Rathore <shubhang92@gmail.com>
2014-08-13 20:37:14 +0530

    Fix script failure if tmux session isn't already running
    If tmux session isn't already running in the background,
    master-script will exit as there won't be any tmux session
    to run actual compilation scripts on.
    
    - Added instructions to set up a crontab job for automation
    - Added initial copyright notice

--- master-script.sh
+++ master-script.sh
@@ -1,3 +1,24 @@
+# Android Compilation Script
+#
+# This script automates the process of compiling android
+# operating system on build servers
+#
+# Copyright (C) 2014 Shubhang Rathore
 
-# Building Galaxy Grand i9082
+
+# Use crontab to run master-script.sh
+#
+#
+# Example crontab job:
+#
+# 30 18 * * * ~/build-scripts/master-script.sh
+
+
+
+# Start tmux detached from main terminal
+# If the session already exists, this command will
+# be ignored
+tmux new-session -d -s build-aogp
+
+# Compiling Galaxy Grand i9082
 tmux send-keys './device-scripts/build-i9082.sh' enter
