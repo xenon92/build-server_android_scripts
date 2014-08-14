82845d05c5d9ea8efada76d611a6c9d92553d3ea
Shubhang Rathore <shubhang92@gmail.com>
2014-08-14 07:31:28 -0400

    Give absolute path to device specific scripts

--- master-script.sh
+++ master-script.sh
@@ -35,4 +35,4 @@
 tmux new-session -d -s build-rom
 
 # Compiling Galaxy Grand i9082
-tmux send-keys './device-scripts/build-i9082.sh' enter
+tmux send-keys '~/build-scripts/device-scripts/build-i9082.sh' enter
